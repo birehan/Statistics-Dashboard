d846f8f81f864ebab7811f8b7d360b37d956e7e5
birehan <birehananteneh4@gmail.com>
2023-08-27 16:29:38 +0300

    fix(birehan): fix errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-// const withMT = require("@material-tailwind/react/src/utils/withMT");
-
-module.exports = withMT({
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
